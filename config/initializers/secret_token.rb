bbd5795c25cc1301479877d82f6b8933f13b04b9
Mark L. Chang <mark.chang@gmail.com>
2011-03-01 20:51:07 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smoothmoves::Application.config.secret_token = '49e13473f25d7c1d96d6b174b0ee91e5f53b549a3b35147357dac9fff325a1f9bd4056d3b8b81b554b67652407d19f836edc2200cd7a3754018487b398b8c4fd'
